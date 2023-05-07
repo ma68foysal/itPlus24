0bea1bd2c142e213efb2376aa20796ae6eea907a
Jakir Ahmad <you@example.com>
2023-05-08 00:05:48 +0600

    [responsiveness]

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,11 +7,11 @@ const Footer = () => {
         <>
         <div className='container footer '>
             <div className='row'>
-                <div className='col-lg-6'>
+                <div className='col-lg-6 footer-content'>
                     <img className='footer-logo' src='/LOGO-PNG.png' alt="" />
                     <p className='footer-descriptin mt-20'>IT-plus24 is an experienced IT service provider that focuses on various industries and serves customers ranging from small to large companies. Our expertise includes designing, building and maintaining IT environments.</p>
                 </div>
-                <div className='col-lg-6'>
+                <div className='col-lg-6 footer-content'>
                         <h2 className='footer-contact-head'>Contact</h2>
                         <h3 className='footer-contact-title mt-20'>Address</h3>
                         <p className='footer-contact-des'>IJplein 420 1021 LW Amsterdam</p>
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -10,37 +10,37 @@ const Portfolio = () => {
                     <p className='portfolio-desc text-center'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
                 </div>
                 <div className="row">
-                    <div className="col-4">
-                        <div className="portfol-content">
+                    <div className="col-4 col-12 " >
+                        <div className="portfol-content m-mt-30">
                             <h3 className='cart-title'>Web Developement</h3>
                             <p className='cart-desc'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy</p>
                         </div>
                     </div>
-                    <div className="col-4">
-                        <div className="portfol-content">
+                    <div className="col-4 col-12 ">
+                        <div className="portfol-content m-mt-30">
                             <h3 className='cart-title'>Web Developement</h3>
                             <p className='cart-desc'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy</p>
                         </div>
                     </div>
-                    <div className="col-4">
-                        <div className="portfol-content">
+                    <div className="col-4 col-12 ">
+                        <div className="portfol-content m-mt-30">
                             <h3 className='cart-title'>Web Developement</h3>
                             <p className='cart-desc'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy</p>
                         </div>
                     </div>
-                    <div className="col-4 ">
+                    <div className="col-4 col-12 ">
                         <div className="portfol-content mt-30">
                             <h3 className='cart-title'>Web Developement</h3>
                             <p className='cart-desc'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy</p>
                         </div>
                     </div>
-                    <div className="col-4 ">
+                    <div className="col-4 col-12 ">
                         <div className="portfol-content mt-30">
                             <h3 className='cart-title'>Web Developement</h3>
                             <p className='cart-desc'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy</p>
                         </div>
                     </div>
-                    <div className="col-4 ">
+                    <div className="col-4 col-12 ">
                         <div className="portfol-content mt-30">
                             <h3 className='cart-title'>Web Developement</h3>
                             <p className='cart-desc'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy</p>
